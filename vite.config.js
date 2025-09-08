291ebd74f84e98a4171cd22d4336989dc6089ea2
shri-reaper <shrisanthironman@gmail.com>
2025-09-08 15:50:21 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: './index.html', // make sure index.html is at the root
+    },
+  },
+  resolve: {
+    alias: {
+      '@': '/src', // optional, for cleaner imports
+    },
+  },
+});
