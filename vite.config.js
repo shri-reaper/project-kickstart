87029e5f7ac3206c5dffe9e6806d319c1956bdc0
shri-reaper <shrisanthironman@gmail.com>
2025-09-08 16:07:26 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'dist',        // Vercel default output folder for static sites
     rollupOptions: {
-      input: './index.html', // make sure index.html is at the root
-    },
+      input: './index.html' // entry point for Vite
+    }
   },
   resolve: {
     alias: {
-      '@': '/src', // optional, for cleaner imports
-    },
-  },
+      '@': '/src'
+    }
+  }
 });
